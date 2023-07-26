e845113e646775040cb1c4cad9d0f7b6174d3ecf
Pedro Arriola <pedroparty310@gmail.com>
2023-07-25 20:54:58 -0600

    :pencil: - Primeras pruebas con Smack

--- src/main/java/com/xmpp_client/App.java
+++ src/main/java/com/xmpp_client/App.java
@@ -1,4 +1,12 @@
 package com.xmpp_client;
+import java.io.IOException;
+
+import org.jivesoftware.smack.AbstractXMPPConnection;
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.SmackException;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.tcp.XMPPTCPConnection;
+import org.jivesoftware.smack.tcp.XMPPTCPConnectionConfiguration;
 
 /**
  * Hello world!
@@ -12,6 +20,21 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        XMPPTCPConnectionConfiguration config = XMPPTCPConnectionConfiguration.builder()
+            .setUsernameAndPassword("username", "password")
+            .setHost("your_host")
+            .setPort(5222)
+            .setSecurityMode(ConnectionConfiguration.SecurityMode.disabled)
+            .build();
+
+        AbstractXMPPConnection conn = new XMPPTCPConnection(config);
+        try {
+            conn.connect();
+            if(conn.isConnected()) {
+                System.out.println("Connected");
+            }
+        } catch (SmackException | IOException | XMPPException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
