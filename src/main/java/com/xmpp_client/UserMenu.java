b8989f093dbdadb42f74b66145c7a31bf6208bf0
PEDRO PABLO ARRIOLA JIMENEZ <pedroparty310@gmail.com>
2023-08-14 21:36:23 -0600

    :pencil: - Lista de contactos y agregar nuevo contacto implementado

--- src/main/java/com/xmpp_client/UserMenu.java
+++ src/main/java/com/xmpp_client/UserMenu.java
@@ -4,7 +4,11 @@ import org.jivesoftware.smack.AbstractXMPPConnection;
 import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.packet.Presence;
 import org.jivesoftware.smack.roster.Roster;
+import org.jivesoftware.smack.roster.RosterEntry;
 import org.jivesoftware.smackx.iqregister.AccountManager;
+import org.jxmpp.jid.BareJid;
+import org.jxmpp.jid.impl.JidCreate;
+import org.jxmpp.stringprep.XmppStringprepException;
 import org.jivesoftware.smack.SmackException;
 
 import java.util.Scanner;
@@ -25,10 +29,12 @@ public class UserMenu {
             System.out.println("=== User Menu ===");
             System.out.println("1. Show My Profile");
             System.out.println("2. Update Presence Status");
-            System.out.println("3. Manage Messages");
-            System.out.println("4. Manage Contacts");
-            System.out.println("5. Delete Account");
-            System.out.println("6. Log Out");
+            System.out.println("3. List of Contacts");
+            System.out.println("4. Add new Contact");
+            System.out.println("5. Manage Messages");
+            System.out.println("6. Manage Contacts");
+            System.out.println("7. Delete Account");
+            System.out.println("8. Log Out");
             System.out.print("\nEnter your choice: ");
 
             int choice = scanner.nextInt();
@@ -41,16 +47,22 @@ public class UserMenu {
                     updatePresence(scanner);
                     break;
                 case 3:
-                    // Manage Contacts
+                    listContacts();
                     break;
                 case 4:
-                    
+                    addNewContact(scanner);
                     break;
                 case 5:
+                
+                    break;
+                case 6:
+                
+                    break;
+                case 7:
                     deleteUser();
                     exit = true;
                     break;
-                case 6:
+                case 8:
                     logOut();
                     exit = true;
                     break;
@@ -117,6 +129,53 @@ public class UserMenu {
             System.out.println("\nFailed to delete account: " + e.getMessage());
         }
     }
+
+    private void addNewContact(Scanner scanner) {
+        System.out.print("Enter the JID of the contact you want to add: ");
+        String jidString = scanner.next();
+        System.out.print("Enter a name for this contact: ");
+        String name = scanner.next();
+    
+        // Convert the JID string to a BareJid object
+        BareJid jid;
+        try {
+            jid = JidCreate.bareFrom(jidString);
+        } catch (XmppStringprepException e) {
+            System.out.println("\nInvalid JID format.\n");
+            return;
+        }
+    
+        // Get the roster associated with the connection
+        Roster roster = Roster.getInstanceFor(connection);
+    
+        // Create the new entry and request subscription
+        try {
+            roster.createItemAndRequestSubscription(jid, name, null);
+            System.out.println("Contact added successfully!");
+        } catch (SmackException.NotLoggedInException | SmackException.NoResponseException | 
+                 XMPPException.XMPPErrorException | SmackException.NotConnectedException | 
+                 InterruptedException e) {
+            System.out.println("Failed to add contact: " + e.getMessage());
+        }
+    }
+
+    private void listContacts() {
+        Roster roster = Roster.getInstanceFor(connection);
+        System.out.println("=== Contacts List ===");
+        System.out.println(String.format("%-25s %-20s %-15s", "Username", "Status", "Mode"));
+        System.out.println(String.format("%-25s %-20s %-15s", "--------", "------", "----"));
+        for (RosterEntry entry : roster.getEntries()) {
+            BareJid jid = entry.getJid();
+            String user = jid.getLocalpartOrNull().toString();
+            Presence presence = roster.getPresence(jid);
+            String status = presence.getStatus() != null ? presence.getStatus() : "N/A";
+            String mode = presence.getMode() != null ? presence.getMode().toString() : "available";
+            System.out.println(String.format("%-25s %-20s %-15s\n", user, status, mode));
+        }
+    }
+    
+    
+    
     
 
 }
